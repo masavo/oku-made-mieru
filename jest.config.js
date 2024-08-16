719568c42acbde5f5e8348cbedae5ddb5bd9865d
masavo <hiroki.masanori06@gmail.com>
2024-08-16 19:45:25 +0900

    Use tsconfig.json for transform

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,12 @@ module.exports = {
   testEnvironment: "node",
   moduleFileExtensions: ["ts", "js"],
   transform: {
-    "^.+\\.ts$": "ts-jest",
+    "^.+\\.ts$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+      },
+    ],
   },
   testMatch: ["**/test/**/*.test.ts"],
 };
